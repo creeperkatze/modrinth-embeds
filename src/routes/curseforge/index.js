541836e17ccb907388ea35bf2f839aff40a921eb
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2026-01-24 17:48:08 +0100

    Remove api key check

--- src/routes/curseforge/index.js
+++ src/routes/curseforge/index.js
@@ -2,12 +2,8 @@ import express from "express";
 import * as curseforgeController from "../../controllers/curseforgeController.js";
 import * as cardController from "../../controllers/cardController.js";
 import * as badgeController from "../../controllers/badgeController.js";
-import curseforgeClient from "../../services/curseforgeClient.js";
-import { requireApiKey } from "../../middleware/apiConfig.js";
 
 const router = express.Router();
-
-router.use(requireApiKey("CurseForge", curseforgeClient, "CURSEFORGE_API_KEY"));
 
 // Slug lookup route (resolve CurseForge slugs to IDs)
 router.get("/curseforge/lookup/:slug", curseforgeController.getCfSlugLookup);
